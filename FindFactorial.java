34e25bf30902fc9c94b62fff0b35a9b9ae706da5
SachinDJ04 <128888007+SachinDJ04@users.noreply.github.com>
2023-12-05 15:25:36 +0530

    second commit

--- FindFactorial.java
+++ FindFactorial.java
@@ -5,7 +5,7 @@ public class FindFactorial {
 	public static void main(String[] args) {
 		
 		System.out.println("Factorial : " + new FindFactorial().factorial(7));
-		
+		System.out.println("Hello");
 	}
 	
 	int factorial(int num) {
